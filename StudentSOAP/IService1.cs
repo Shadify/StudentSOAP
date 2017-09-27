08bb65d939656d91a2209aaa5e9d0b3ec4e90a82
Shadify <boomkin1234@hotmail.com>
2017-09-27 09:06:38 +0200

    Add project files.

---
+++ StudentSOAP/IService1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace StudentSOAP
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        void AddStudent(string name, int semester, int age);
+
+        [OperationContract]
+        string FindStudent(string name);
+
+        [OperationContract]
+        void DeleteStudent(Student studentA);
+
+        [OperationContract]
+        void EditStudent(Student studentA, Student studentB);
+
+        [OperationContract]
+        List<Student> GetAllStudent();
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Semester { get; set; }
+        public int Age { get; set; }
+    }
+
+
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ StudentSOAP/Service1.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace StudentSOAP
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : Student, IService1
+    {
+        static List<Student> listOfStudents = new List<Student>();
+
+        public void AddStudent(string name, int semester, int age)
+        {
+            Student newStudent = new Student();
+            newStudent.Name = name;
+            newStudent.Semester = semester;
+            newStudent.Age = age;
+            listOfStudents.Add(newStudent);
+        }
+
+        public void DeleteStudent(Student studentA)
+        {
+            listOfStudents.Remove(studentA);
+        }
+
+        public void EditStudent(Student Before, Student After)
+        {
+            listOfStudents.Remove(Before);
+            listOfStudents.Add(After);
+        }
+
+        public string FindStudent(string name)
+        {
+            var find = listOfStudents.Find(Student => Student.Name == name);
+            
+        }
+
+        public List<Student> GetAllStudent()
+        {
+            return listOfStudents;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
